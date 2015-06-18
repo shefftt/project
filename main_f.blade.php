deec893c2b25febb9079a294ab7784a3fc97b99f
ahmed hmed <ahmed7med@gmail.com>
2015-06-18 13:28:07 +0300

    Create main_f.blade.php

---
+++ main_f.blade.php
@@ -0,0 +1,14 @@
+
+<div class="conent">
+
+	@include('footer.'.$page)
+
+</div>
+<footer>
+	<ul>
+		<li><a href="privicy" >الخصوصية</a></li>
+		<li><a href="terms">الاتفاقيه</a></li>
+		<li><a href="help">المساعده</a></li>
+		<li><a href="about">من نحن</a></li>
+	</ul>
+</footer>
