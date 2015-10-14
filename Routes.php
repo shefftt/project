cd4b8506078a4513e9f12c35248e60409d1c54db
ahmed hmed <ahmed7med@gmail.com>
2015-10-14 20:46:46 +0300

    Update Routes.php

--- Routes.php
+++ Routes.php
@@ -1,26 +1,52 @@
 <?php
 
-
 /*
-هذا المسارات يقوم الملف باختبار الرابط مثلا 
-example.com/help
-هل هذا المسار موجود اذا كان موجوداضمن المصفوفة التى تسمى 
-$page 
-اذا كان موجود داخل المصفوفة يذهب للمسار للصفحة  
-main_f
-الموجوده داخل المجلد
-footer
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|يقوم المستخدم بعرض فورم الدخول اذا كان اسم المستخدم وكلة السر صحيتين يقوم بتحويلة للصفحه الداش بورد 
+اما اذا كان خطاء يقوم الموقع بعرض رساله خطاء
 
+
+ولكن مايحدث شى غريب يقوم الموقع بتوجيهك لصفحه الدخول مرة اخرى اذا كانتا كلمه السر واسم المستخدم صحيحتين
+|
 */
-Route::get('/{page}' ,function($page)
+
+Route::get('/', function()
 {
-	$pages = array('privicy','terms','help','about');
-	if(in_array( $page , $pages))
-	//
-     return View::make('footer.main_f')->with('page' , $page);
- 
+	return View::make('hello');
 });
 
+Route::get('login',function()
+{
+	return '<form action="login2" method="POST">
+	         '.Form::token().'
+	         <input type="text" name="username">
+			 <input type="text" name="password">
+			 <input type="submit" value = "insert">
+			
+			 </form>';
+});
 
+///////////////////////////////////////////////////////////////////////////////////// End of  login viwe
+/*
+   المسار الذى يتم ارسال المعلومات علية
+*/
+Route::post('login2',function()
+{
+	$method  = array('username' => Input::get('username') , 'password' => Input::get('password'));
+               if (Auth::attempt($method)) 
+               	return Redirect::intended('dashbord');
+		else
+			return "Sorry username or password wrong ...";
+		});
+/////////////////////////////////////////////////////////////////////////////////// End of auth function
+/*
+   المسار المحمى
+   */
+Route::get('dashbord', array('before'=>'auth', function()
+{
+	return "auth";
+}));
 
-?>
+////////////////////////////////////////////////////////////////////////////// End of Hidden area
